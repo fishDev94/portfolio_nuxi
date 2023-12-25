bd9d2789d5f6ac630bd369a6d847b6ef617630ea
MarcoGuglielmino <marco.guglielmino@musement.com>
2023-12-25 16:53:33 +0100

    feat: update aboutme section

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -3,6 +3,7 @@ defineProps<{
   info: {
     description: string;
     img: string;
+    urls: { name: string; url: string }[];
   };
 }>();
 </script>
@@ -10,8 +11,14 @@ defineProps<{
 <template>
   <div class="about-me">
     <div class="about-me__container">
-      <h3>About Me</h3>
+      <h2>About Me</h2>
       <div class="about-me__container--text" v-html="info.description"></div>
+      <p>Urls:</p>
+      <ul class="about-me__container--contacts">
+        <li v-for="url in info.urls">
+          <a :href="url.url">{{ url.name }}</a>
+        </li>
+      </ul>
       <img :src="useAssets(info.img)" :alt="info.img" />
     </div>
   </div>
@@ -22,18 +29,24 @@ defineProps<{
 
 .about-me {
   @include start-from(desktop) {
-    min-height: calc(100dvh - 77px);
+    min-height: calc(100dvh - 77px - 70px);
   }
 
-  min-height: calc(100dvh - 50px);
+  min-height: calc(100dvh - 50px - 70px);
+  display: flex;
 
   &__container {
-    width: 80%;
+    // max-width: 1440px;
+    // min-width: 500px;
+    display: flex;
+    flex-direction: column;
+    gap: 18px;
+    padding: 40px;
     height: 80%;
     margin: 50px auto;
     background-color: rgb(var(--neutral));
 
-    h3 {
+    h2 {
       color: rgb(var(--secondary));
     }
 
