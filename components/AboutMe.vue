822cf9707948e15a1bf0b49ba22c8dd09c1170ba
MarcoGuglielmino <marco.guglielmino@musement.com>
2023-12-27 17:09:06 +0100

    feat: complete about me mobile ver section

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -12,6 +12,10 @@ defineProps<{
   <div class="about-me">
     <div class="about-me__container">
       <h2>About Me</h2>
+      <div class="about-me__container--avatar">
+        <div class="about-me__container__filter"></div>
+        <img :src="useAssets(info.img)" :alt="info.img" />
+      </div>
       <div class="about-me__container--text" v-html="info.description"></div>
       <p>Urls:</p>
       <ul class="about-me__container--contacts">
@@ -19,7 +23,6 @@ defineProps<{
           <a :href="url.url">{{ url.name }}</a>
         </li>
       </ul>
-      <img :src="useAssets(info.img)" :alt="info.img" />
     </div>
   </div>
 </template>
@@ -34,13 +37,15 @@ defineProps<{
 
   min-height: calc(100dvh - 50px - 70px);
   display: flex;
+  align-items: center;
+  justify-content: center;
 
   &__container {
     // max-width: 1440px;
     // min-width: 500px;
     display: flex;
     flex-direction: column;
-    gap: 18px;
+    gap: 32px;
     padding: 40px;
     height: 80%;
     margin: 50px auto;
@@ -50,15 +55,41 @@ defineProps<{
       color: rgb(var(--secondary));
     }
 
-    &--text {
-      color: rgb(var(--secondary));
-      .link {
-        color: red;
+    &--avatar {
+      width: 150px;
+      margin: 0 auto;
+      border-radius: 8px;
+      overflow: hidden;
+
+      img {
+        width: 100%;
       }
     }
 
-    img {
-      width: 40px;
+    &__filter {
+      position: absolute;
+      z-index: 3;
+      background-color: rgb(var(--primary));
+      opacity: 0.3;
+      width: 150px;
+      height: 150px;
+      border-radius: 8px;
+    }
+
+    &--text {
+      color: rgb(var(--secondary));
+    }
+
+    &--contacts {
+      display: flex;
+      gap: 12px;
+      justify-content: space-evenly;
+
+      a {
+        color: rgb(var(--secondary));
+        font-weight: 600;
+        text-decoration: underline;
+      }
     }
   }
 }
