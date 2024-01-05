385257354d20abf0b7d682c3d62d19744247263e
marco94gug <marco.guglielmino94@gmail.com>
2024-01-05 11:27:23 +0100

    feat: introducing nuxt-img

--- components/Atomic/Card.vue
+++ components/Atomic/Card.vue
@@ -9,7 +9,13 @@ defineProps<{
 
 <template>
   <div :class="['card', type]">
-    <img class="card__img" :src="useAssets(data.img)" alt="lorem" />
+    <!-- <img class="card__img" :src="useAssets(data.img)" alt="lorem" /> -->
+    <nuxt-img
+      class="card__img"
+      :src="data.img"
+      alt="Gallery Picture"
+      format="webp"
+    />
     <div class="card__description">
       <div class="card__description--title">
         <h3>{{ data.title }}</h3>
