8a52739678827b0c645838c0086de9b000c6d7ba
marco94gug <marco.guglielmino94@gmail.com>
2024-01-05 13:03:49 +0100

    feat: remove commented code

--- components/Atomic/youtubeVideo.vue
+++ components/Atomic/youtubeVideo.vue
@@ -9,13 +9,6 @@ defineProps<{
 <template>
   <section class="youtube">
     <h3>{{ title }}</h3>
-    <!-- <iframe
-      src="https://www.youtube.com/embed/urfqORZL1u0?si=uPiTaBimtAHxAHYq"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen
-    ></iframe> -->
     <LiteYouTubeEmbed
       id="urfqORZL1u0"
       title="Marco Guglielmino - Alive (Official Video)"
