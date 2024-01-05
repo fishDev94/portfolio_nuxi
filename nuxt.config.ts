cef732302f899df45e0cc419702f86e22a753710
marco94gug <marco.guglielmino94@gmail.com>
2024-01-05 12:00:38 +0100

    feat: test env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/robots"],
   devtools: { enabled: true },
   css: ["@/assets/styles/globals.scss", "primeicons/primeicons.css"],
+  image: {
+    provider: process.env.VERCEL_ENV ? "vercel" : "ipx",
+  },
 });
