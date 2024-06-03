437a344561178ddfb34771f391e0d8100691194a
MarcoGuglielmino <marco.guglielmino@musement.com>
2024-06-03 11:17:00 +0200

    add array spread operator

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 const pippo = 'pippo';
 
 const pippoArray = ['pluto', 'fish'];
+
+console.log(pippoArray.push('qualcosa'));
+const newArray = [...pippoArray, 'ciao'];
+//OUTPUT: [ 'pluto', 'fish', 'ciao' ]
